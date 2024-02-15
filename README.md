69706352e3053128781a7ded2668347bd11595ef
Blankeos <carloantonioct@gmail.com>
2024-02-15 23:23:47 +0800

    feat: greeting.

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
-# README
+<h1>Wails + Solid + TS + Tailwind Template</h1>
 
-## About
+<h2>Table of Contents</h2>
 
-This is the Wails Solid Template created by [Carlo](https://github.com/blankeos).
+- [🤔 About](#%F0%9F%A4%94-about)
+- [👨‍💻 Live Development](#%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB-live-development)
+- [🔨 Building](#%F0%9F%94%A8-building)
+
+## 🤔 About
+
+This is the Wails Solid Template created by [Carlo](https://github.com/blankeos) for his Desktop application projects.
+This is currently a work in progress.
 
 **Technologies Used**:
 
 - Wails (Go)
 - SolidJS
 - TailwindCSS
+- Extra things for DevX:
+  - prettier + prettier-plugin-tailwindcss
+  - PNPM (If you wish to change, make sure to change `wails.json`)
 
-Added these for DevX:
-
-- prettier + prettier-plugin-tailwindcss
-- PNPM (If you wish to change, make sure to change `wails.json`)
-
-## Live Development
+## 👨‍💻 Live Development
 
 To run in live development mode, run `wails dev` in the project directory. This will run a Vite development
 server that will provide very fast hot reload of your frontend changes. If you want to develop in a browser
 and have access to your Go methods, there is also a dev server that runs on http://localhost:34115. Connect
 to this in your browser, and you can call your Go code from devtools.
 
-## Building
+## 🔨 Building
 
 To build a redistributable, production mode package, use `wails build`.
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,62 @@
-import { createSignal } from "solid-js";
+import { createMemo, createSignal } from "solid-js";
 import solidLogo from "./assets/solid.svg";
+import wailsLogo from "./assets/wails.png";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Greet } from "wailsjs/go/main/App";
 
 function App() {
   const [count, setCount] = createSignal(0);
+
+  const [name, setName] = createSignal("");
+
   const [greetResult, setGreetResult] = createSignal("");
 
   function greet() {
-    Greet("carlo").then((result) => setGreetResult(result));
+    if (name() === "") return;
+
+    Greet(name()).then((result) => setGreetResult(result));
   }
 
   return (
     <>
-      <div>
+      <div class="flex items-center justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
+        <a href="https://wails.io/" target="_blank">
+          <img src={wailsLogo} class="logo wails" alt="Wails logo" />
+        </a>
       </div>
-      <h1>Vite + Solid</h1>
+      <h1>Vite + Solid + Wails + Tailwind</h1>
+
+      <div class="h-5" />
       <div>
+        <div class="flex justify-center gap-x-2">
+          <input
+            name="name"
+            placeholder="Name"
+            class="rounded-md p-2"
+            onInput={(e) => setName(e.target.value)}
+          />
+          <button onClick={greet}>Greet</button>
+        </div>
+
         {greetResult()}
-        <button onClick={greet}>Greet</button>
       </div>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+
+      <div class="h-5" />
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count()}
+      </button>
+
+      <div class="h-2" />
+      <p class="text-gray-400">
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
     </>
   );
